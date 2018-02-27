ee5d240c514dcaa8ec1c125da165b45070bafc73
julian ladjani <julian.ladjani@epitech.eu>
2018-02-27 22:16:09 +0100

    Real Test for objdump

---
+++ Tests/nm-objdump/test-objdump-header.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+import unittest
+import executetest
+
+
+class TestObjdumpHeaderSimpleExecutableFile(unittest.TestCase):
+
+    def setUp(self):
+        self.return_value, self.stdout, self.stderr = executetest.execute_program("./my_objdump", "my_objdump")
+
+    def test_file_format(self):
+        self.assertTrue(self.stdout.contains("\nmy_objdump:     file format elf64-x86-64\n"))
+
+    def test_file_architecture(self):
+        self.assertTrue(self.stdout.contains("\narchitecture: i386:x86-64, flags 0x00000150:\n"))
+
+    def test_file_flags(self):
+        self.assertTrue(self.stdout.contains("\nHAS_SYMS, DYNAMIC, D_PAGED\n"))
+
+    def test_start_address(self):
+        self.assertTrue(self.stdout.contains("\nstart address 0x0000000000000a80\n\n"))
+
+
+if __name__ == '__main__':
+    executetest.execute_test()
