84b1fb6ee3242e0043ef65c712acdb21a4c8e087
julian ladjani <julian.ladjani@epitech.eu>
2018-03-06 00:54:22 +0100

    Philosophers project, test-argument-error

---
+++ Tests/philosophers/test-arg-errors.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+
+import unittest
+import executetest
+
+
+class TestExecutableArgumentErrorCases(unittest.TestCase):
+
+    def test_no_philosopher_number_arg_error(self):
+        return_value, stdout, stderr = executetest.execute_program("./philo", "-e 3")
+        if return_value == -1 and stderr == "Timeout Reached":
+            self.fail("Timeout Reached")
+        self.assertTrue(return_value == 84)
+
+    def test_no_eat_max_times_number_arg_error(self):
+        return_value, stdout, stderr = executetest.execute_program("./philo", "-p 3")
+        if return_value == -1 and stderr == "Timeout Reached":
+            self.fail("Timeout Reached")
+        self.assertTrue(return_value == 84)
+
+    def test_negative_philosopher_number_error(self):
+        return_value, stdout, stderr = executetest.execute_program("./philo", "-p -3")
+        if return_value == -1 and stderr == "Timeout Reached":
+            self.fail("Timeout Reached")
+        self.assertTrue(return_value == 84)
+
+    def test_negative_max_eat_times_number_error(self):
+        return_value, stdout, stderr = executetest.execute_program("./philo", "-e -3")
+        if return_value == -1 and stderr == "Timeout Reached":
+            self.fail("Timeout Reached")
+        self.assertTrue(return_value == 84)
+
+    def test_zero_philosopher_number_error(self):
+        return_value, stdout, stderr = executetest.execute_program("./philo", "-p 0")
+        if return_value == -1 and stderr == "Timeout Reached":
+            self.fail("Timeout Reached")
+        self.assertTrue(return_value == 84)
+
+    def test_zero_max_eat_times_number_error(self):
+        return_value, stdout, stderr = executetest.execute_program("./philo", "-e 0")
+        if return_value == -1 and stderr == "Timeout Reached":
+            self.fail("Timeout Reached")
+        self.assertTrue(return_value == 84)
+
+    def test_too_much_arg_error(self):
+        return_value, stdout, stderr = executetest.execute_program("./philo", "-p 3 -p 4 -e 5")
+        if return_value == -1 and stderr == "Timeout Reached":
+            self.fail("Timeout Reached")
+        self.assertTrue(return_value == 84)
+
+    def test_wrong_philosopher_number_error(self):
+        return_value, stdout, stderr = executetest.execute_program("./philo", "-p 1 -e 5")
+        if return_value == -1 and stderr == "Timeout Reached":
+            self.fail("Timeout Reached")
+        self.assertTrue(return_value == 84)
+
+
+if __name__ == '__main__':
+    executetest.execute_test()
