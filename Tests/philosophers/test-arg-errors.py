2c18c03725211279d96f676d374ce1bb93ceaa4f
julian ladjani <julian.ladjani@epitech.eu>
2018-03-23 02:46:14 +0100

    add error message

--- Tests/philosophers/test-arg-errors.py
+++ Tests/philosophers/test-arg-errors.py
@@ -10,49 +10,49 @@ class TestExecutableArgumentErrorCases(unittest.TestCase):
         return_value, stdout, stderr = executetest.execute_program("./philo", "-e 3")
         if return_value == -1 and stderr == "Timeout Reached":
             self.fail("Timeout Reached")
-        self.assertTrue(return_value == 84)
+        self.assertTrue(return_value == 84, str(return_value) + "instead of 84")
 
     def test_no_eat_max_times_number_arg_error(self):
         return_value, stdout, stderr = executetest.execute_program("./philo", "-p 3")
         if return_value == -1 and stderr == "Timeout Reached":
             self.fail("Timeout Reached")
-        self.assertTrue(return_value == 84)
+        self.assertTrue(return_value == 84, str(return_value) + "instead of 84")
 
     def test_negative_philosopher_number_error(self):
         return_value, stdout, stderr = executetest.execute_program("./philo", "-p -3")
         if return_value == -1 and stderr == "Timeout Reached":
             self.fail("Timeout Reached")
-        self.assertTrue(return_value == 84)
+        self.assertTrue(return_value == 84, str(return_value) + "instead of 84")
 
     def test_negative_max_eat_times_number_error(self):
         return_value, stdout, stderr = executetest.execute_program("./philo", "-e -3")
         if return_value == -1 and stderr == "Timeout Reached":
             self.fail("Timeout Reached")
-        self.assertTrue(return_value == 84)
+        self.assertTrue(return_value == 84, str(return_value) + "instead of 84")
 
     def test_zero_philosopher_number_error(self):
         return_value, stdout, stderr = executetest.execute_program("./philo", "-p 0")
         if return_value == -1 and stderr == "Timeout Reached":
             self.fail("Timeout Reached")
-        self.assertTrue(return_value == 84)
+        self.assertTrue(return_value == 84, str(return_value) + "instead of 84")
 
     def test_zero_max_eat_times_number_error(self):
         return_value, stdout, stderr = executetest.execute_program("./philo", "-e 0")
         if return_value == -1 and stderr == "Timeout Reached":
             self.fail("Timeout Reached")
-        self.assertTrue(return_value == 84)
+        self.assertTrue(return_value == 84, str(return_value) + "instead of 84")
 
     def test_too_much_arg_error(self):
         return_value, stdout, stderr = executetest.execute_program("./philo", "-p 3 -p 4 -e 5")
         if return_value == -1 and stderr == "Timeout Reached":
             self.fail("Timeout Reached")
-        self.assertTrue(return_value == 84)
+        self.assertTrue(return_value == 84, str(return_value) + "instead of 84")
 
     def test_wrong_philosopher_number_error(self):
         return_value, stdout, stderr = executetest.execute_program("./philo", "-p 1 -e 5")
         if return_value == -1 and stderr == "Timeout Reached":
             self.fail("Timeout Reached")
-        self.assertTrue(return_value == 84)
+        self.assertTrue(return_value == 84, str(return_value) + "instead of 84")
 
 
 if __name__ == '__main__':
