8db787e4c404d538a4261f87c44867d33d64b0dd
julian ladjani <julian.ladjani@epitech.eu>
2018-06-21 05:59:19 +0200

    Objdump tests fix objdump

--- Tests/nm-objdump/test-nm-execute.py
+++ Tests/nm-objdump/test-nm-execute.py
@@ -7,11 +7,9 @@ import executetest
 
 class TestNmExecutableFile(unittest.TestCase):
 
-    def setUp(self):
+    def test_output_diff_hard_test_objdump_exe(self):
         self.return_value, self.stdout, self.stderr = executetest.execute_program("./my_nm", "my_objdump")
         self.gnureturn_value, self.gnustdout, self.gnustderr = executetest.execute_program("nm", "my_objdump")
-
-    def test_output_diff_hard_test(self):
         if self.return_value == -1 and self.stderr == "Timeout Reached":
             self.fail("Timeout Reached")
         self.stdout_list = str.split(self.stdout, '\n')
@@ -20,6 +18,16 @@ class TestNmExecutableFile(unittest.TestCase):
         self.stdout_list.sort()
         self.assertTrue(self.stdout_list == self.gnustdout_list)
 
+    def test_output_diff_hard_test_nm_exe(self):
+        self.return_value, self.stdout, self.stderr = executetest.execute_program("./my_nm", "my_objdump")
+        self.gnureturn_value, self.gnustdout, self.gnustderr = executetest.execute_program("nm", "my_objdump")
+        if self.return_value == -1 and self.stderr == "Timeout Reached":
+            self.fail("Timeout Reached")
+        self.stdout_list = str.split(self.stdout, '\n')
+        self.gnustdout_list = str.split(self.gnustdout, '\n')
+        self.gnustdout_list.sort()
+        self.stdout_list.sort()
+        self.assertTrue(self.stdout_list == self.gnustdout_list)
 
 if __name__ == '__main__':
     executetest.execute_test()
--- Tests/nm-objdump/test-objdump-execute.py
+++ Tests/nm-objdump/test-objdump-execute.py
@@ -8,7 +8,7 @@ class TestObjdumpExecutableFile(unittest.TestCase):
 
     def setUp(self):
         self.return_value, self.stdout, self.stderr = executetest.execute_program("./my_objdump", "my_objdump")
-        self.gnureturn_value, self.gnustdout, self.gnustderr = executetest.execute_program("objdump", "my_objdump")
+        self.gnureturn_value, self.gnustdout, self.gnustderr = executetest.execute_program("objdump", "-fs my_objdump")
 
     def test_output_diff_hard_test(self):
         if self.return_value == -1 and self.stderr == "Timeout Reached":
